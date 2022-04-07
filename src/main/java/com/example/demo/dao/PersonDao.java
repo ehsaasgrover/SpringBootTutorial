836217480f1892d27fd6cdae39227c1fc90ade15
ehsaasgrover <ehsaas.grover@hotmail.com>
2022-04-07 12:14:41 +1200

    [001] Implementing RESTful services

---
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -0,0 +1,29 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    // Inserts person with both id and name
+    int insertPerson(UUID id, Person person);
+
+    // Inserts person without id and generates an id for them
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePerson(UUID id, Person person);
+
+
+}
